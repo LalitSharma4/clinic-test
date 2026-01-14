0e416f5a380ee6a16c14e20887d480b33eee8106
LalitSharma4 <iamlalitsharma4@gmail.com>
2026-01-14 16:08:13 +0530

    change data

--- api/views.py
+++ api/views.py
@@ -5,7 +5,7 @@ from .serializers import ProductSerializer
 PRODUCTS = [
     {"id": 1, "name": "Laptop", "price": 65000},
     {"id": 2, "name": "Phone", "price": 25000},
-    {"id": 3, "name": "Tablet", "price": 30000},
+   
 ]
 
 class ProductListAPI(APIView):
